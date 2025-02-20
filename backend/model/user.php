17ef2483c989d54e6cd6d4891e1e4835807956f5
Lililatortue <will.descoteaux@gmail.com>
2025-02-19 23:40:53 -0500

    add login and hashing and salting

--- backend/model/user.php
+++ backend/model/user.php
@@ -4,12 +4,14 @@ include "../dbdata/dBConnection.php";
 include "config.php"; 
 
 function findOneUser($data){
-    
+
     try{
         $db=DatabaseConnection::getInstance();
-        $stmt = $db -> prepare( "SELECT * FROM user WHERE id=:id" );
-        $stmt -> execute([":id"=>$data['id']]);
+        $stmt = $db -> prepare( "SELECT * FROM user WHERE email=:email" );
+        $stmt -> execute([":email"=>$data['email']]);
         $user=$stmt->fetch(PDO::FETCH_ASSOC);
+        
+        
     } catch (Exception $e) {
         //error handling server error
         http_response_code(500);
@@ -31,13 +33,16 @@ function fetchAllUser(){
         $stmt -> execute();
         //sa retourne une list de user
         $users=$stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($users as &$user) {
+            unset($user['salt']);
+        }
     } catch(PDOException $e){
         http_response_code(500);
         return ["erreur: impossible d'etablir une connection avec la database."];
     }
     if($users){
         http_response_code(200);
-        return [$users];
+        return ["user :"=>$users];
     } else {
         http_response_code(204);//204 est retourne quand la requete a reussi mais aucun contenu est retourner
         return ["no data found"];
@@ -48,10 +53,20 @@ function createUser($data){
     
     try{
         $db= DatabaseConnection::getInstance();
-        $stmt = $db -> prepare("INSERT INTO user(username, email, pays, age) 
-                                    VALUES (:username,:email, :pays, :age)");
-        $bool=$stmt -> execute([":username" => $data['username'], ":email" => $data['email'], 
-                                ":pays" =>$data['pays'],":age" => $data['age']]);
+
+        //salt and hash pssword
+        $salt = base64_encode(random_bytes(16));
+        $iterations = 600000;
+        $hash = hash_pbkdf2("sha256",$data['password'],$salt,$iterations,20);
+
+        $stmt = $db -> prepare("INSERT INTO user(username, email, pays, age, password, salt ) 
+                                    VALUES (:username,:email, :pays, :age, :password, :salt)");
+        $bool=$stmt -> execute([":username" => $data['username'],
+                                 ":email" => $data['email'], 
+                                ":pays" =>$data['pays'],
+                                ":age" => $data['age'],
+                                ":password" =>$hash,
+                                ":salt" =>$salt]);
     } catch(Exception $e){
         //error handling server error
         http_response_code(500);
@@ -65,17 +80,13 @@ function createUser($data){
         return ["erreur est survenu durant la creation."];
     }
 }
-//a modifier car il faut toute les parametre pour quelle fonctionne
+
 function updateUser($data){
     //bad error handling
     try{
-        $db= DatabaseConnection::getInstance();
-        $stmt = $db -> prepare("UPDATE user SET username=:username, email=:email, pays=:pays, age=:age WHERE id=:id");
-        $bool = $stmt -> execute([":id" => $data["id"],
-                                  ":username" => $data["username"], 
-                                  ":email" => $data["email"],
-                                  ":pays" => $data["pays"],
-                                  ":age" => $data["age"]]);
+        $db= DatabaseConnection::getInstance(); 
+        $stmt = $db -> prepare(createString($data));
+        $bool = $stmt -> execute(createExecute($data));
     } catch(Exception $e){
         http_response_code(500);
         return ["erreur: erreur logique  updateUser."];
@@ -93,7 +104,7 @@ function updateUser($data){
 function deleteUser($data){
     $db= DatabaseConnection::getInstance();
     try{
-        $stmt = $db -> prepare("DELETE FROM user WHERE id=:id");
+        $stmt = $db -> prepare("DELETE FROM user WHERE email=:email");
         $bool = $stmt -> execute(["id"=>$data['id']]);     
     } catch (Exeception $e) {
         http_response_code(500);
@@ -108,4 +119,33 @@ function deleteUser($data){
     }
 }
 
+function login($data){
+    $user=findOneUser($data);
+    
+    $iterations = 600000;
+    $hash = hash_pbkdf2("sha256",$data['password'],$user["salt"],$iterations,20);
+    
+    if($hash==$user['password']){
+        http_response_code(200);
+        return $user;
+    } else {
+        http_response_code(401);
+        return [("error : invalid credentials")];
+    }
+}
+
+ function createString($data){
+    $usedData=$data;
+    unset($usedData['email']);
+    $columns = implode(", ", array_map(fn($key) => "$key = :$key", array_keys($data)));
+    
+    return "UPDATE user SET $columns WHERE email = :email";
+ }
+ function createExecute($data){
+    $query=[];
+    foreach($data as $key=>$value){
+        $query[":".$key]=$value;
+    }
+    return $query;
+}
 ?>
