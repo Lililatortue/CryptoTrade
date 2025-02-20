2fd39d0cb8d73d78329e677fed5ea57bc45cc437
Lililatortue <will.descoteaux@gmail.com>
2025-02-19 23:41:27 -0500

    added file

---
+++ backend/model/crypto.php
@@ -0,0 +1,23 @@
+<?php
+include "../dbdata/database.php";
+
+function findOne(){
+    
+}
+
+function fetchAll(){
+
+}
+
+function createCrypto(){
+
+}
+
+function updateCrypto(){
+
+}
+
+function deleteCrypto(){
+    
+}
+?>
