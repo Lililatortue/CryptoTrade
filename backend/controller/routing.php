490def043622025592d1739c7530a453497c2bf4
Lililatortue <will.descoteaux@gmail.com>
2025-03-05 23:29:29 -0500

    modification du routing j'ai integrer la classe endpointObserver et logger

--- backend/controller/routing.php
+++ backend/controller/routing.php
@@ -1,8 +1,15 @@
 
 <?php
 include "../model/user.php";
+include "../sanitizer/endpointObserver.php";
+include "../sanitizer/logger.php";
 
-header("Content-Type: application/json");
+
+//variable init
+$observer = new endpointObserver();
+
+// ROUTE CREATION
+header("Content-Type: application/json"); 
 
 $methods = $_SERVER['REQUEST_METHOD'];
 $uri = strtok($_SERVER['REQUEST_URI'], '?');
@@ -11,22 +18,52 @@ $basePath = "/backend/controller/routing.php/";
 if (strpos($uri, $basePath) === 0) {
     $uri = substr($uri, strlen($basePath));
 }
-
 $param=empty($_GET) ? (empty($_POST) ?  json_decode(file_get_contents('php://input'), true) : $_POST  ) : $_GET;
 
 //use est un capture dans ce context c'est inutile de l'utiliser mais j'aime  comment sa ressemble
-$action = function($data) use($methods,$uri){
+$action = function($data) use($methods,$uri,$observer){
   
     $route = "$methods/$uri";
-    
+    $logger = new logger('APICALLS','../sanitizer/logs/',logType::ACTION );
+    $logger ->setFalse();
     switch($route){
-        //route user
-        case "POST/user/add": return createUser($data);
-        case "PATCH/user/update": return updateUser($data);
-        case "GET/user/fetchAll": return fetchAllUser();
-        case "GET/user/findOne": return findOneUser($data);
-        case "DELETE/user/delete":  return deleteUser($data);
-        case "GET/user/login": return login($data);
+        //route user            
+        case "POST/user/add":   $logger->startTimer();
+                                if(empty($resultat=$observer->userInfoPolicies($data))){
+                                  $resultat=createUser($data); 
+                                  $logger ->setTrue();
+                                }
+                                $logger->EndTimer();
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data); 
+                                return $resultat;
+        case "PATCH/user/update":$logger->startTimer();
+                                if(empty($resultat=$observer->userInfoPolicies($data))){
+                                  $resultat=updateUser($data);
+                                  $logger->setTrue();
+                                }
+                                $logger->EndTimer();  
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data); 
+                                return $resultat;
+        case "GET/user/fetchAll":$logger->startTimer();
+                                $resultat=fetchAllUser(); 
+                                $logger->EndTimer();          
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data);
+                                return $resultat;
+        case "GET/user/findOne":$logger->startTimer();   
+                                $resultat=findOneUser($data);
+                                $logger->EndTimer(); 
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data);
+                                return findOneUser($data);
+        case "DELETE/user/delete":$logger->startTimer(); 
+                                $resultat=deleteUser($data);
+                                $logger->EndTimer(); 
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data);
+                                return $resultat;
+        case "GET/user/login":  $logger->startTimer();                           
+                                $resultat=login($data);
+                                $logger->EndTimer();
+                                $logger->logging($_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT'],$route,$resultat,$data);
+                                return $resultat;
         
         //route crypto
         case "POST/crypto/add": return createCrypto($data);
@@ -35,11 +72,11 @@ $action = function($data) use($methods,$uri){
         case "GET/crypto/findOne": return findOneCrypto($data);
         case "DELETE/crypto/delete": return deleteCrypto($data);
         
-
         default: {http_response_code(404); return ["erreur, route non-existante"];}//bad request;
     }
 };
-
+//sanitize data
+$observer->sanitize($param);
 echo json_encode($action($param));
 ?>
 
